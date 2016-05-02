2711680f73a1d547d7234e3e85fa196acf33a0c4
wodrow <1173957281@qq.com>
2016-05-02 11:31:59 +0800

    add colume icon to table menu

--- console/migrations/m160501_104918_alter_menu_add_field_icon.php
+++ console/migrations/m160501_104918_alter_menu_add_field_icon.php
@@ -6,11 +6,11 @@ class m160501_104918_alter_menu_add_field_icon extends Migration
 {
     public function up()
     {
-
+        $this->addColumn("{{%menu}}", 'icon', 'string(50)');
     }
 
     public function down()
     {
-        
+        $this->dropColumn("{{%menu}}", 'icon');
     }
 }
